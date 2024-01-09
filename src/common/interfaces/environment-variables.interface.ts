c0eeea7b0acc5198b57ca55152ac7ff9549b7704
ivanvilashek <ivan.vilashek@gmail.com>
2024-01-10 01:47:32 +0200

    Enabled CORS, created google-aouth guard, environment variable interface

---
+++ src/common/interfaces/environment-variables.interface.ts
@@ -0,0 +1,12 @@
+export interface EnvironmentVariables {
+  POSTGRES_HOST: string
+  POSTGRES_PORT: string
+  POSTGRES_NAME: string
+  POSTGRES_USER: string
+  POSTGRES_PASSWORD: string
+  DATABASE_URL: string
+  PORT: string
+  GOOGLE_CLIENT_ID: string
+  GOOGLE_CLIENT_SECRET: string
+  GOOGLE_REDIRECT_URL: string
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import { EnvironmentVariables } from '@app/common'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const configService = app.get(ConfigService<EnvironmentVariables>)
+
+  const PORT = configService.get<number>('PORT')
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: ['PUT', 'GET', 'POST', 'PATCH'],
+    credentials: true,
+  }
+
+  app.enableCors(corsOptions)
+
+  await app.listen(PORT)
 }
+
 bootstrap()
